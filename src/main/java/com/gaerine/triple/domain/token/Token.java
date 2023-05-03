5f26ad02ba10654c8e8839050271807ccc2552a4
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-03 16:32:50 +0900

    [CREATE] Token DTO 추가

---
+++ src/main/java/com/gaerine/triple/domain/token/Token.java
@@ -0,0 +1,19 @@
+package com.gaerine.triple.domain.token;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class Token {
+    private Long id;
+    private Long member_id;
+    private String access_token;
+    private String refresh_token;
+    private Date created_date;
+    private Date modified_date;
+}

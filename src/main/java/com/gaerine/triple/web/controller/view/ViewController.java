2924a8127484ac27ec0982872f1a640135e4b15a
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-07 22:08:10 +0900

    [CREATE] View 컨트롤러 생성
    RESTController를 사용하기위해 Get View 는 ViewController에서 관리

---
+++ src/main/java/com/gaerine/triple/web/controller/view/ViewController.java
@@ -0,0 +1,27 @@
+package com.gaerine.triple.web.controller.view;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.UUID;
+
+@Controller
+@Slf4j
+public class ViewController {
+
+    // register.html 이동
+    @GetMapping("/member/register")
+    public String moveReg(Model model){
+        String state = UUID.randomUUID().toString();
+        model.addAttribute("state",state);
+        return "member/register";
+    }
+
+    // map 이동
+    @GetMapping("/map")
+    public String moveBoard(){
+        return "board/map";
+    }
+}

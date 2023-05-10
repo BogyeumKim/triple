57f6e14736495ef6ad80e043a0fe36234390c68b
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-11 00:36:32 +0900

    [UPDATE] 일정 생성시 페이지에 선택한 도시 지도 뿌려주기
    컨트롤러에서 lat , lng 값을 가져오고 페이지 구글지도 뿌리는 함수에 lat, lng값을 뿌려줌

--- src/main/java/com/gaerine/triple/web/controller/board/BoardController.java
+++ src/main/java/com/gaerine/triple/web/controller/board/BoardController.java
@@ -1,29 +1,36 @@
 package com.gaerine.triple.web.controller.board;
 
+import com.gaerine.triple.ApiKey;
 import com.gaerine.triple.domain.board.Capital;
 import com.gaerine.triple.domain.board.TripBoard;
+import com.gaerine.triple.domain.board.TripBoardAndCapital;
 import com.gaerine.triple.domain.board.World;
 import com.gaerine.triple.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 @Slf4j
-@RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService service;
+    private final ApiKey apiKey;
+
+    @Autowired
+    public BoardController(BoardService service, ApiKey apiKey) {
+        this.service = service;
+        this.apiKey = apiKey;
+    }
 
     @GetMapping("/write")
     public String moveFindWolrd(Model model){
@@ -57,6 +64,19 @@ public class BoardController {
         return tripBoard.getBoard_id() != null ? ResponseEntity.ok().body(tripBoard) : ResponseEntity.badRequest().build();
     }
 
+    @GetMapping("/board/{boardId}")
+    public String readBoard(@PathVariable Long boardId,Model model){
+        Optional<TripBoardAndCapital> board = Optional.ofNullable(service.getBoardCapitalById(boardId));
 
+        if(board.isEmpty()) {
+            // 에러 view 만들어서 리턴시키기 , 로그인 검증도 추가하자)
+            return null;
+        }
+
+        model.addAttribute("board",board.get());
+        model.addAttribute("key",apiKey.getApiKey());
+
+        return "board/map";
+    }
 
 }

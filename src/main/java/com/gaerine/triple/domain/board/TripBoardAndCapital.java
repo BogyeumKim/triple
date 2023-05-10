41b3cc5f13617429460dc7a047ef0ff8538020a8
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-10 23:51:54 +0900

    [CREATE] TripBoard , Capital 한번에 가져오는 객체 추가

---
+++ src/main/java/com/gaerine/triple/domain/board/TripBoardAndCapital.java
@@ -0,0 +1,13 @@
+package com.gaerine.triple.domain.board;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TripBoardAndCapital {
+    private TripBoard tripBoard;
+    private Capital capital;
+}

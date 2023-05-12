a367ae9e8571daefdf998afa3815d3d449416525
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-12 15:02:07 +0900

    [CREATE] 선택한 장소 객체 추가

---
+++ src/main/java/com/gaerine/triple/domain/board/SelectPlace.java
@@ -0,0 +1,11 @@
+package com.gaerine.triple.domain.board;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class SelectPlace {
+    private Long placeId;
+    private String placeName;
+}

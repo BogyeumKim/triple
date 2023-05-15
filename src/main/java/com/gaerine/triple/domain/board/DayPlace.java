33e61d8a5aa734c79c74eceee8fc5eadf8f0118a
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-15 17:56:00 +0900

    [UPDATE] DayPlace 객체 Map to String 변경
    json형태 map이 아닌 string으로 받아옴

--- src/main/java/com/gaerine/triple/domain/board/DayPlace.java
+++ src/main/java/com/gaerine/triple/domain/board/DayPlace.java
@@ -12,9 +12,9 @@ import java.util.Map;
 public class DayPlace {
     private Long day_id;
     private Long board_id;
-    private Map<String, String> day1;
-    private Map<String, String> day2;
-    private Map<String, String> day3;
-    private Map<String, String> day4;
-    private Map<String, String> day5;
+    private String day1;
+    private String day2;
+    private String day3;
+    private String day4;
+    private String day5;
 }

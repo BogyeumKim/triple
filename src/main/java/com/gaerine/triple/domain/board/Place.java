7b900b19aa75f6d1b28a7085e75afc08a5d92263
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-05-22 13:30:25 +0900

    [UPDATE] Validation 그래들 추가

--- src/main/java/com/gaerine/triple/domain/board/Place.java
+++ src/main/java/com/gaerine/triple/domain/board/Place.java
@@ -1,15 +1,23 @@
 package com.gaerine.triple.domain.board;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @ToString
 public class Place {
     private Long id;
+    @NotNull
     private Long capital_id;
+    @NotNull
     private String korea_name;
+    @NotNull
     private String category;
+    @NotNull
     private Double lat;
+    @NotNull
     private Double lng;
 }

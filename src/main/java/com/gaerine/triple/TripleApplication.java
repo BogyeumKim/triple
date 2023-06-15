207477c554981ccc282a6befd206c3bfc3bce164
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-06-15 22:39:40 +0900

    [UPDATE] 배포 환경 재 업데이트

--- src/main/java/com/gaerine/triple/TripleApplication.java
+++ src/main/java/com/gaerine/triple/TripleApplication.java
@@ -1,9 +1,12 @@
 package com.gaerine.triple;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.gaerine.triple.mapper")
 public class TripleApplication {
 
 	public static void main(String[] args) {

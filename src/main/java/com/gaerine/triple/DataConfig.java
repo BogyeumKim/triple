c087f11f7b8a6308db7c87f3b512ec467749d442
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-08-27 18:59:22 +0900

    [CREATE] Local , Cloude DB 분리

---
+++ src/main/java/com/gaerine/triple/DataConfig.java
@@ -0,0 +1,61 @@
+package com.gaerine.triple;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class DataConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.local.hikari")
+    public DataSource localDataSource(){
+        HikariDataSource build = DataSourceBuilder.create().type(HikariDataSource.class).build();
+        return build;
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.cloude.hikari")
+    public DataSource cloudeDataSource(){
+        HikariDataSource build = DataSourceBuilder.create().type(HikariDataSource.class).build();
+        return build;
+    }
+
+    @Bean
+    @DependsOn({"localDataSource","cloudeDataSource"})
+    public DataSource routingDataSource(
+            @Qualifier("localDataSource") DataSource localDataSource,
+            @Qualifier("cloudeDataSource") DataSource cloudeDataSource) {
+
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+
+        Map<Object, Object> map = new HashMap<>() {};
+        map.put("local",localDataSource);
+        map.put("cloude",cloudeDataSource);
+
+        routingDataSource.setTargetDataSources(map);
+        routingDataSource.setDefaultTargetDataSource(cloudeDataSource);
+
+        return routingDataSource;
+    }
+
+
+    @Bean
+    @Primary
+    public LazyConnectionDataSourceProxy dataSource(DataSource routingDataSource){
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
